33cda35b54de9a938ae693ea1c6b26ec7c3cdac1
eis group at farmers insurance <eis@farmersinsurance.com>
2017-02-01 13:19:44 -0800

    Initial setup of spring boot app

---
+++ src/main/java/io/pivotal/learn/test/smells/TestSmellToyApplication.java
@@ -0,0 +1,12 @@
+package io.pivotal.learn.test.smells;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSmellToyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSmellToyApplication.class, args);
+	}
+}
